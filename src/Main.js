090d7861d773433c26d58286badebea949b72fab
TheoGandon <theo.gandon@gmail.com>
2023-12-02 14:32:20 +0100

    Ajout pc 3D

--- src/Main.js
+++ src/Main.js
@@ -11,12 +11,12 @@ function Main() {
   return (
     <div className="App">
       <Navbar />
-      <div className="wave-container">
+      {/* <div className="wave-container">
         <motion.div className="wave" animate={{ rotate: 360 }} transition={{ duration: 2, repeat: Infinity }}>
           <motion.div className="wave-inner" animate={{ y: [0, -10, 0] }} transition={{ duration: 1.5, repeat: Infinity, ease: 'linear' }} />
         </motion.div>
         <img src={Profile} alt="Profile" className="image" />
-      </div>
+      </div> */}
       <div className="text-container">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
       </div>
